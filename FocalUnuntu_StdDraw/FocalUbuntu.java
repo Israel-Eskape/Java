76d9a0e2706b0fccb67cc9a2b47518d29639eeb7
Israel <isra.ixoye@gmail.com>
2020-10-12 13:44:34 -0500

    Se agregaron poligonos y los ojos para dibujar la parte superior de la cara

--- FocalUnuntu_StdDraw/FocalUbuntu.java
+++ FocalUnuntu_StdDraw/FocalUbuntu.java
@@ -38,6 +38,14 @@ public class FocalUbuntu{
         pts.add(new Point2D.Double(31.63,67.42));
         pts.add(new Point2D.Double(33.06,77.1));
         poligono(pts,0);
+        //lado izquierdo del ojo
+        StdDraw.setPenColor(new Color(206,76,49));
+        pts.add(new Point2D.Double(28.13,71.95));
+        pts.add(new Point2D.Double(25.43,61.85));
+        pts.add(new Point2D.Double(26.23,55.46));
+        pts.add(new Point2D.Double(27.02,56.08));
+        pts.add(new Point2D.Double(31.47,67.01));
+        poligono(pts, 0);
         //Centro
         pts.add(new Point2D.Double(40.2,86.18));
         pts.add(new Point2D.Double(33.06,77.1));
@@ -69,9 +77,25 @@ public class FocalUbuntu{
         pts.add(new Point2D.Double(46.89,64.74));
         pts.add(new Point2D.Double(50.23,68.65));
         poligono(pts, 0);
-
+        //Ojo Izquierdo
+        StdDraw.setPenColor(StdDraw.WHITE);
+        pts.add(new Point2D.Double(29.25,70.30));
+        pts.add(new Point2D.Double(27.98,64.94));
+        pts.add(new Point2D.Double(28.77,61.44));
+        pts.add(new Point2D.Double(31.31,67.8));
+        poligono(pts, 1);
+        //Ojo Derecho
+        pts.add(new Point2D.Double(50.30,68.65));
+        pts.add(new Point2D.Double(47.21,64.75));
+        pts.add(new Point2D.Double(46.89,58.76));
+        pts.add(new Point2D.Double(48.48,60.82));
+        pts.add(new Point2D.Double(52.14,60.61));
+        pts.add(new Point2D.Double(58.18,67.83));
+        poligono(pts, 1);
 
         //Ear Rigth
+        StdDraw.setPenColor(new Color(206,76,49));
+
         pts.add(new Point2D.Double(83.6,94.4));
         pts.add(new Point2D.Double(76.4,96.7));
         pts.add(new Point2D.Double(69.15,90.51));
@@ -86,7 +110,25 @@ public class FocalUbuntu{
         pts.add(new Point2D.Double(63.11,74.02));
         pts.add(new Point2D.Double(67.7,84.1));
         poligono(pts, 0);
-        
+        //derecha del ojo derecho 
+        pts.add(new Point2D.Double(67.7,84.1));
+        pts.add(new Point2D.Double(63.11,74.02));
+        pts.add(new Point2D.Double(58.18,67.83));
+        pts.add(new Point2D.Double(52.14,60.61));
+        pts.add(new Point2D.Double(48.48,60.82));
+        pts.add(new Point2D.Double(46.89,58.76));
+        pts.add(new Point2D.Double(46.10,53.60));
+        pts.add(new Point2D.Double(49.12,49.69));
+        pts.add(new Point2D.Double(68.36,53.60));
+        pts.add(new Point2D.Double(69.7,70.72));
+        poligono(pts, 0);
+        //Abajo de la oreja rellena
+        pts.add(new Point2D.Double(69.7,70.72));
+        pts.add(new Point2D.Double(68.36,53.60));
+        pts.add(new Point2D.Double(72.97,54.63));
+        pts.add(new Point2D.Double(74.56,58.76));
+        pts.add(new Point2D.Double(80.00,75.05));
+        poligono(pts, 0);
         
         
 
