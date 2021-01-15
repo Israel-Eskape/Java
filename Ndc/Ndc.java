7e9a9cd682599a01a1cefca85e3bb5db399e598c
Avendaño Martinez Gustavo Israel <isra.ixoye@gmail.com>
2021-01-15 03:21:54 -0600

    Se optimiza y elimina todas las variables que no se ocupan

--- Ndc/Ndc.java
+++ Ndc/Ndc.java
@@ -9,15 +9,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
 import java.awt.*;
-import java.awt.geom.*;
 
 
 public class Ndc extends JFrame {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
  
     JLabel jlabelR = new JLabel();
     
@@ -32,7 +26,6 @@ public class Ndc extends JFrame {
 
     JScrollPane jScrollPane;
     Ndc() {
-        //setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1280, 700);
         setResizable(false);
@@ -57,6 +50,7 @@ public class Ndc extends JFrame {
          * Se inicializan los paneles con una posición y un tamaño especifico
          * para cada panel. en este trabajo tiene 6 paneles 
          */
+
         panel0.setLocation(10, 10);
         panel0.setSize(800, 600);
 
@@ -113,9 +107,6 @@ public class Ndc extends JFrame {
     }
 
     public void readFile(String nameFile, JPanel panel) {
-        Double xx =0.0, yy=0.0;
-        double r;
-        Punto p;
         ArrayList<Punto> listPoint = new ArrayList();
         try {
             /**
@@ -127,7 +118,7 @@ public class Ndc extends JFrame {
              */
 
             panelaux.add(panel);
-            r = (double) (panel.getSize().getWidth()/panel.getSize().getHeight());
+            double r = (double) (panel.getSize().getWidth()/panel.getSize().getHeight());
             jlabelR.setText((int)(panel.getSize().getWidth()) + " * "+ (int)(panel.getSize().getHeight())+"  R = "+ r);
             panel.add(jlabelR);
             panel.setBackground(Color.LIGHT_GRAY);
@@ -140,25 +131,21 @@ public class Ndc extends JFrame {
             BufferedReader br = new BufferedReader(file);
             String d;
             while ((d = br.readLine()) != null) {
-                /**Si en la linea que está leyendo encuentra la palabra end entonces va llamar a la función
+                
+                /** Si en la linea que está leyendo encuentra la palabra end entonces va llamar a la función
                  * para que se grafiquén los puntos y si no lo encuentra se seguirán guardando en un arreglo
                  * el cual se repetirá hasta que encuentre el final de linea o end
                  */
                 if (d.equals("end")) {
-
-                    //getPuntos(panel, auxelement);
                     drawImg(panel, listPoint);
-                    //list.clear(); //Limpia el arreglo porque indica que ya graficó los puntos porque encontro un token de fin de linea
                     listPoint.clear();
                 } else {
                     StringTokenizer dato = new StringTokenizer(d, ",");
-                    xx = Double.parseDouble(dato.nextToken().trim());
-                    yy = Double.parseDouble(dato.nextToken().trim());
-                    p = new Punto(xx,yy);
+                    Double xx = Double.parseDouble(dato.nextToken().trim());
+                    Double yy = Double.parseDouble(dato.nextToken().trim());
+                    Punto p = new Punto(xx,yy);
                     listPoint.add(p);
-
                 }
-
             }
             /**
              * Manda el arreglo de los datos que se leyó del archivo y lo manda a la clase
@@ -174,10 +161,9 @@ public class Ndc extends JFrame {
     public void drawImg(JPanel panel, ArrayList<Punto> puntos) {
         int dx0 = 0, dy0 = 0;
         int dx1 = 0, dy1 = 0;
-        int xmax=0,ymax=0;
         try {
-            xmax = (int) panel.getSize().getWidth();
-            ymax = (int) panel.getSize().getHeight();
+            int xmax = (int) panel.getSize().getWidth();
+            int ymax = (int) panel.getSize().getHeight();
             for (int i = 0; i < puntos.size()-1; i++) {
 
                 dx0 = (int) (puntos.get(i).getX() * xmax);
@@ -185,15 +171,9 @@ public class Ndc extends JFrame {
                 dx1 = (int) (puntos.get(i + 1).getX() * xmax);
                 dy1 = (int) (ymax - (puntos.get(i + 1).getY() * ymax));
 
-                //JOptionPane.showMessageDialog(null,"i " + i + ": "+dx0+" , "+dy0+ "  , "+dx1 +" , "+dy1);
                 Thread.sleep(512);
                 panel.getGraphics().drawLine(dx0, dy0, dx1, dy1);
-                
-                
-            }
-        
-//            Thread.sleep(1000);
-
+        }
         } catch (Exception e) {
             JOptionPane.showInternalMessageDialog(null, "Warning in ---> Ndc/drawImg  <---" + e.getMessage());
         }
@@ -205,4 +185,6 @@ public class Ndc extends JFrame {
 }
 /**
  * Israel Eskape
+ * https://github.com/Israel-Eskape/Java/tree/main/Ndc
+ * 
  */
